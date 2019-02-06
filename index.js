e4b8bab454aca321f97967338a410bbf8f733dbb
Kumar Vaibhav <cs18m030@smail.iitm.ac.in>
2019-02-07 01:32:58 +0530

    Remove comments

--- index.js
+++ index.js
@@ -8,23 +8,21 @@ server.listen(port, host, () => {
     console.log('TCP Server is running on port ' + port + '.');
 });
 
-var last='';
+var last={};
 
 server.on('connection', function(sock) {
     console.log('CONNECTED: ' + sock.remoteAddress + ':' + sock.remotePort);
-    //sockets.push(sock);
 
+    // console.log(sock);
+
+    sockPort = sock.remotePort;
+
+    last[sockPort] = '';
 
     sock.on('data', function(req) {
-        //console.log('DATA ' + sock.remoteAddress + ': ' + data);
-        // console.dir("here");
-        // console.dir(req[req.length-1]);
         var json_texts = req.toString().split("\n");
-        //console.dir(json_texts);
-        // console.dir(req[req.length-1]);
-        // console.dir(req.length);
-        json_texts[0] = last+json_texts[0];
-        last = json_texts[json_texts.length-1];
+        json_texts[0] = last[sockPort]+json_texts[0];
+        last[sockPort] = json_texts[json_texts.length-1];
         for(d=0;d<json_texts.length-1;d++)
         {
             var str = json_texts[d];
@@ -49,25 +47,7 @@ server.on('connection', function(sock) {
         console.dir(error);
     });
 
-    // Add a 'close' event handler to this instance of socket
     sock.on('close', function(data) {
-        // let index = sockets.findIndex(function(o) {
-        //     return o.remoteAddress === sock.remoteAddress && o.remotePort === sock.remotePort;
-        // })
-        // if (index !== -1) sockets.splice(index, 1);
         console.log('CLOSED: ' + sock.remoteAddress + ' ' + sock.remotePort);
     });
 });
-
-//app.use(express.bodyParser());
-// app.use(express.json());       // to support JSON-encoded bodies
-// app.use(express.urlencoded());
-// app.use(cors());
-
-// app.post('/revStr', (req, res) => {
-//     // console.dir(req.body);
-    
-//     res.send(data);
-// });
-
-// app.listen(port, () => console.log(`Example app listening on port ${port}!`));
